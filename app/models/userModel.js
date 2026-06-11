9dec1cfffb56120e994074d0f2715f825b345251
SOHAILASIF1 <alyana998877@gmail.com>
2026-06-11 07:00:25 +0500

    auto commit

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -13,6 +13,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["admin", "teacher", "student"],
+    default: "student",
+  },
   createdAt: {
     type: Date,
     default: Date.now,
