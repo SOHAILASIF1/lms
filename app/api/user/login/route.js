55c64ce786086740d844d06c9de7b910d6956311
SOHAILASIF1 <alyana998877@gmail.com>
2026-06-11 06:56:05 +0500

    auto commit

---
+++ app/api/user/login/route.js
@@ -0,0 +1,32 @@
+import User from "@/models/User";
+import {connectDb} from "@/lib/db/mongoose";
+import {NextResponse} from "next/server";
+import bcrypt from "bcryptjs";
+
+import jwt from "jsonwebtoken";
+
+export async function POST(request) {
+    try {
+        connectDb()
+        const {email,password} =await request.json()
+        const user=await User.findOne({email})
+        if (!user) {
+            return NextResponse.json({error:"User Not Found"},{status:404})
+            
+        }
+        const comparePassword=await bcrypt.compare(password,user.password)
+
+        if (!comparePassword) {
+            return NextResponse.json({error:"Invalid Password"},{status:401})
+            
+        }
+
+
+
+
+        
+    } catch (error) {
+        
+    }
+    
+}
