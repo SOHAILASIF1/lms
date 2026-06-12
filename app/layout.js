076c1d795432cfe1c2a33aee7f32750e44c85a5d
SOHAILASIF1 <alyana998877@gmail.com>
2026-06-12 07:13:31 +0500

    auto commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { SessionProvider } from 'next-auth/react'
+import Providers from '../components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-surface text-slate-900`}>
-        <SessionProvider>{children}</SessionProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
