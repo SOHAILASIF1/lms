f7fbfb54384102ba06aaaa45e77778da914d0fa5
SOHAILASIF1 <alyana998877@gmail.com>
2026-06-12 06:31:54 +0500

    auto commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        surface: '#f8fafc',
+        panel: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
